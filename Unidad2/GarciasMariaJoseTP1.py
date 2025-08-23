56bf8903baa83152fd1048996f5ffc45cbaa69d2
MariaJoseGarcias <majoogarcias08@gmail.com>
2025-08-23 17:30:45 -0300

    Create GarciasMariaJoseTP1.py
    Subo práctica unidad 1

---
+++ Unidad2/GarciasMariaJoseTP1.py
@@ -0,0 +1,66 @@
+## Garcias Maria Jose - Tec en Programacion a Distancia - Trabajo Practicon° 1 
+
+##Ejercicio n 1:
+##print("Hola Mundo!")
+
+
+##Ejercicio n 2:
+#nombre = input("Ingrese su nombre: ")
+#print(f"Hola {nombre}!" )
+
+##Ejercicio n 3:
+#nombre = input("Ingrese su nombre: ")
+#apellido = input("Ingrese su apellido: ")
+#edad = input("Ingrese su edad: ")
+#residencia = input("Ingrese su residencia: ")
+#print(f"Soy {nombre} {apellido}, tengo {edad} años, vivo en {residencia}")
+
+##Ejercicio n 4:
+#radio = float (input("Ingrese el radio: "))
+#pi = float (3.14159)
+#area = pi * (radio ** 2)
+#perimetro = pi * radio * 2
+#print (f"El area del circulo es: {area} y su perimeto es: {perimetro}")
+
+##Ejercicio n 5:
+#segundos = int (input("Ingrese la cantidad de segundos a calcular: "))
+#horas = float (segundos / 3600)
+#print(f"La cantidad equivalente en horas es de {horas}")
+
+#Ejercicio n 6:
+#numero = int(input ("Ingrese un numero: "))
+#print(f"{numero} * 0 = {numero*0}")
+#print(f"{numero} * 1 = {numero*1}")
+#print(f"{numero} * 2 = {numero*2}")
+#print(f"{numero} * 3 = {numero*3}")
+#print(f"{numero} * 4 = {numero*4}")
+#print(f"{numero} * 5 = {numero*5}")
+#print(f"{numero} * 6 = {numero*6}")
+#print(f"{numero} * 7 = {numero*7}")
+#print(f"{numero} * 8 = {numero*8}")
+#print(f"{numero} * 9 = {numero*9}")
+#print(f"{numero} * 10 = {numero*10}")
+
+##Ejercicio n 7:
+#numero1 = int(input ("Ingrese un numero distinto de 0: "))
+#numero2 = int(input ("Ingrese otro numero distinto de 0: "))
+#print (f"La Suma de {numero1} + {numero2} = {numero1+numero2}")
+#print (f"La Division de {numero1} / {numero2} = {numero1/numero2}")
+#print (f"La Multiplicacion de {numero1} * {numero2} = {numero1*numero2}")
+#print (f"La Resta de {numero1} - {numero2} = {numero1-numero2}")
+
+
+##Ejercicio n 8:
+#altura = float(input ("Ingrese su altura: "))
+#peso = float(input ("Ingrese su peso: "))
+#print(f"Su IMC es de: {peso/altura**2}")
+
+##Ejercicio n 9:
+#temperatura = float(input ("Ingrese la temperatura en grados Celsius: "))
+#print (f"Su equivalente a grados Fahrenheit es de: {9/5* temperatura +32}")
+
+##Ejercicio n 10:
+#numero1 = int(input ("Ingrese un numero : "))
+#numero2 = int(input ("Ingrese un numero : "))
+#numero3 = int(input ("Ingrese un numero : "))
+#print (f"El promedio de dichos numeros es de: {(numero1+numero2+numero3) / 3}")
